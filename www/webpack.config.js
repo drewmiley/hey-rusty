ae9ad7cf43c7b66cb4f944b6ea1d1888324490d7
Drew Miley <drew.miley@dwp.gsi.gov.uk>
2018-12-13 10:41:46 +0000

    React and rust working in perfect harmony

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -2,10 +2,29 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
+  target: "web",
   entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.js",
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.wasm']
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
   },
   mode: "development",
   plugins: [
